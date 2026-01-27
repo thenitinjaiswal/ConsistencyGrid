1b067c101071f028f6a531529c44ae1b3ae393f2
Nitin Jaiswal <nitinjaiswal242004@gmail.com>
2026-01-27 22:08:38 +0530

    refactor: update wallpaper sync logic with enhanced logging

--- src/utils/sendWallpaperToAndroid.js
+++ src/utils/sendWallpaperToAndroid.js
@@ -1,13 +1,8 @@
-export function sendWallpaperToAndroid(data, type = "url") {
+export function sendWallpaperToAndroid(wallpaperUrl) {
     if (typeof window !== "undefined") {
-        if (window.Android) {
-            if (type === "base64" && window.Android.saveWallpaper) {
-                console.log("📱 Sending Base64 wallpaper to Android");
-                window.Android.saveWallpaper(data);
-            } else if (type === "url" && window.Android.saveWallpaperUrl) {
-                console.log("📱 Sending wallpaper URL to Android:", data);
-                window.Android.saveWallpaperUrl(data);
-            }
+        if (window.Android && window.Android.saveWallpaperUrl) {
+            console.log("📱 Sending wallpaper URL to Android:", wallpaperUrl);
+            window.Android.saveWallpaperUrl(wallpaperUrl);
         } else {
             console.log("🌐 Not running inside Android app or bridge not found");
         }
